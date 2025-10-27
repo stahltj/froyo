e92887925b58ac984c0d0327caf7577e7540fc1a
Tyler <tjstahl02@gmail.com>
2025-10-27 14:35:43 -0600

    completed assignment

--- index.js
+++ index.js
@@ -9,14 +9,10 @@ function getFlavors() {
 function tallyFlavors(arrayOfFlavors) {
   let flavorCounts = {};
   arrayOfFlavors.forEach((flavor) => {
-    // maskes sure theres not an empty value for flavor
     if (flavor.trim().toLowerCase().length > 0) {
-      // Check if the current 'flavor' is already a key in flavorCounts
       if (flavorCounts[flavor.trim().toLowerCase()]) {
-        // If the key exists, increment its current count by 1
         flavorCounts[flavor.trim().toLowerCase()] += 1;
       } else {
-        // If the key does not exist, initialize it with a count of 1
         flavorCounts[flavor.trim().toLowerCase()] = 1;
       }
     }
